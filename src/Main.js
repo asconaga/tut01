0850d815c2b407e9674673e7cf071c8f50d02c12
asconaga <asconaga@gmail.com>
2022-07-14 22:48:20 +0100

    new project

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Main = () => {
+    const name = "Kenny";
+
+    return (
+        <main>
+        <p> Hello {name}!</p>
+        </main>
+    )
+}
+
+export default Main
