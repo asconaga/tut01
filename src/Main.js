ffc84c92a708b583ac42b630db01a6bdcdf59281
asconaga <109314922+asconaga@users.noreply.github.com>
2022-07-15 10:02:50 +0100

    looking at usestate

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import useState from 'react';
 
 const Main = () => {
-    const name = "Kenny";
+    const name = "Kennybiscuit";
+
+    const [name, setName] = useState('Kenny');
+
+    const handleClick = () => {
+        alert('you clicked me');
+    }
 
     return (
         <main>
             <p> Hello <b>{name}</b> its nice to see you!</p>
+            <button> onClick={handleClick}</button>
         </main>
     )
 }
